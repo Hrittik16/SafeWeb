1b8d891977c2a4e1e59d90cf250d7ab0d9dad469
hrittik16 <hrittikdas16@gmail.com>
2021-01-16 15:54:49 +0530

    Python script added for preparing dataset

---
+++ Chrome Extension/script.js
@@ -0,0 +1,16 @@
+addLayer(document.body)
+
+function addLayer(element) {
+    // This if statement checks if the current element
+    // has any child nodes
+    if(element.hasChildNodes()) {
+        // This line recursively calls the function for
+        // each child nodes
+        element.childNodes.forEach(addLayer)
+    }
+    else if(element.nodeType === Text.TEXT_NODE) { 
+        // The else if statement checks if the current
+        // element is a text element 
+
+    }
+}
---
+++ script.py
@@ -0,0 +1,11 @@
+# This python script is to parse the dataset and 
+# remove all unnecessary characters
+
+infile = open("cuss_words.json", "r")
+outfile = open("cuss_words_updated.json", "w")
+for line in infile:
+	for word in line:
+		if word is "}" or word is "{":
+			outfile.write("")
+		else:
+			outfile.write(word)
